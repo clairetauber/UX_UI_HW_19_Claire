585d0b671bc0bce5a791f8b35ff932aa63ea63ae
clairetauber <ctaubs97@gmail.com>
2022-07-19 18:22:12 -0700

    updated hw

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('[popup-open]').on('click', function() {
+        var popup_name = $(this).attr('popup-open');
+ $('[popup-name="' + popup_name + '"]').fadeIn(300);
+    });
+ 
+    $('[popup-close]').on('click', function() {
+ var popup_name = $(this).attr('popup-close');
+ $('[popup-name="' + popup_name + '"]').fadeOut(300);
+    });
+ 
+});
